0bb775faa134c553e29bf4c13a45e798a4ec2e66
conevrenk <155632852+conevrenk@users.noreply.github.com>
2024-10-05 22:07:36 +0300

    footer and policy add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
+import Footer from './components/Footer'
 import Header from './components/Header'
+import Policy from './components/Policy'
 
 const App = () => {
   return (
     <div>
       <Header />
       <h1>hello</h1>
+      <Policy/>
+      <Footer/>
     </div>
   )
 }
